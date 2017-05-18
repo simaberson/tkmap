13fa1c26a6158ca91f89dd1cc6f0ac19edb77951
simaberson <sim.aberson@noaa.gov>
2017-05-18 15:17:51 -0400

    Add files via upload

---
+++ tkmap.sh
@@ -0,0 +1,9 @@
+rm out*
+rm hurrloc*
+rm points*
+xtkmap
+ls -1 points1 points2 points3 > points
+ls -1 hurrloc1 hurrloc2 hurrloc3 > hurrloc
+/adaptive/applications/grads2.1/bin/grads -l -g 900x675+50+50 << GRADSEND
+run plotdrops.gs
+GRADSEND
